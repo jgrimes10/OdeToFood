e1f487cd8cbcc0058f5f7cc49a6745505ccec4a0
Justin Grimes <jgrimes@relias.com>
2019-11-06 09:59:06 -0500

    Initial commit. Create Data and Web projects. Add initial models

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private readonly List<Restaurant> _restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant {Id = 1, Name = "Scott's Pizza", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Tersiguels", Cuisine = CuisineType.French},
+                new Restaurant {Id = 3, Name = "Mango Grove", Cuisine = CuisineType.Indian},
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
